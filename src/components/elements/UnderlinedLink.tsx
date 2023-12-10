eddb15de16851996f7a95e265d35fa731e47e76a
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2023-12-10 17:32:51 +0530

    UnderlineLink color changed

--- src/components/elements/UnderlinedLink.tsx
+++ src/components/elements/UnderlinedLink.tsx
@@ -1,28 +1,26 @@
-import UnstyledLink, { UnstyledLinkProps } from '@/components/elements/UnstyledLink'
+import UnstyledLink, {
+  UnstyledLinkProps,
+} from "@/components/elements/UnstyledLink";
 
-import { twclsx } from '@/libs/twclsx'
+import { twclsx } from "@/libs/twclsx";
 
 export interface UnderlineLinkProps extends UnstyledLinkProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const UnderlineLink: React.FC<UnderlineLinkProps> = ({ children, ...props }) => {
+const UnderlineLink: React.FC<UnderlineLinkProps> = ({
+  children,
+  ...props
+}) => {
   return (
     <UnstyledLink
-      className={twclsx(
-        'inline-flex',
-        'relative border-b-2 border-dotted font-semibold',
-        'border-main-3 dark:border-main-2',
-        'after:absolute after:-bottom-0.5 after:left-0 hover:after:w-full',
-        'after:h-[3px] after:w-0 after:transition-all after:duration-300',
-        'after:bg-primary-3 dark:after:bg-primary-2',
-        'text-primary-4 dark:text-primary-2 hover:text-primary-2 dark:hover:text-primary-3'
-      )}
+      className={twclsx("relative", "inline-block", "text-[#20a4f3]", "group", "no-underline")}
       {...props}
     >
       {children}
+      <span className="absolute inset-x-0 bottom-0 h-0.5 bg-[#20a4f3] transform origin-right scale-x-0 transition-transform duration-250 ease-out group-hover:origin-left group-hover:scale-x-100"></span>
     </UnstyledLink>
-  )
-}
+  );
+};
 
-export default UnderlineLink
+export default UnderlineLink;
